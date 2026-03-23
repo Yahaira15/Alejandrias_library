9cce53258b271163958d40681cd35fa22d7416fa
1201kevBN <bohorquezkevin12@gmail.com>
2026-03-23 16:06:51 -0500

    Modulo de Inicio de sesion, con protección de rutas mediante un guardia de autenticación.

--- alejandrias_frontend/src/app/Login/login/login.ts
+++ alejandrias_frontend/src/app/Login/login/login.ts
@@ -1,11 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  mostrarPassword: boolean = false;
+
+  loginData = {
+    login: '',
+    usuario_password: ''
+  };
+
+  errorMensaje: string = '';
+
+  iniciarSesion(form: any) {
+    console.log('LOGIN DATA:', this.loginData);
+
+    if (form.invalid) {
+      this.errorMensaje = 'Completa todos los campos';
+      return;
+    }
+
+    this.loginData.login = this.loginData.login.trim();
+
+    this.http.post<any>('http://localhost:8000/api/login', this.loginData)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+
+          localStorage.setItem('usuario', JSON.stringify(res.usuario));
+
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+        console.error(err);
+
+        if (err.error && err.error.mensaje) {
+          this.errorMensaje = err.error.mensaje;
+        } else {
+          this.errorMensaje = 'Error en el servidor';
+        }
+      }
+      });
+  }
 }
--- alejandrias_frontend/src/app/app.routes.ts
+++ alejandrias_frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './Login/login/login';
 import { Register } from './Login/register/register';
+import { Home } from './Home/home/home';
+import { authGuard } from './guards/auth-guard';
  
 export const routes: Routes = [
     {path: 'login', component: Login },
-    {path: 'register', component: Register }
+    {path: 'register', component: Register },
+    {path: 'home', component: Home,canActivate: [authGuard] },
 ];
