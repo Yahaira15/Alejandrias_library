cef2a004d036bff44f501a978fc8a0160e299e87
Yahaira Tellez <yahairatellez2007@gmail.com>
2026-04-14 14:12:37 -0500

    fix: arreglo_para_entrar_a_los_foros_y_visualizar_publicaciones

--- alejandrias_frontend/src/app/app.routes.ts
+++ alejandrias_frontend/src/app/app.routes.ts
@@ -7,12 +7,16 @@ import { EditarForo } from './Foro/editar-foro/editar-foro';
 import { authGuard } from './guards/auth-guard';
 import { Home } from './Home/home/home';
 import { Perfil } from './perfil/perfil';
+import { VerPublicacionComponent } from './Foro/ver-publicacion/ver-publicacion';
+import { VerForoComponent } from './Foro/ver-foro/ver-foro';
  
 export const routes: Routes = [
     {path: 'home',component:Home},
     {path: 'login', component: Login },
     {path: 'register', component: Register },
     {path: 'foros/crear', component: CrearForo},
+    { path: 'foros/:foro_id', component: VerForoComponent },
+    { path: 'publicaciones/:publicacion_id', component: VerPublicacionComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full'},
     { path: 'foros', component: ListaForos, canActivate: [authGuard] },
     { path: 'foros/editar/:id', component: EditarForo, canActivate: [authGuard] },
